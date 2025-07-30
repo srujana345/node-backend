9203f064d5576bec9e86ff7a88f5590d3a0fd101
srujana345 <sai.srujana144@gmail.com>
2025-07-30 11:07:52 +0530

    new

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
+import dotenv from "dotenv";
 import orderRoute from "./routes/orderRoute.js"
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+const dbUser=encodeURIComponent(process.env.DBUSER);
+const dbPass=encodeURIComponent(process.env.DBPASS);
 // mongoose
 //   .connect("mongodb://localhost:27017/grietdb")
 //   .then(() => {
@@ -18,7 +22,7 @@ app.use(cors());
 //     console.error("Error connecting to MongoDB:", err);
 //   });
   mongoose
-  .connect("mongodb+srv://saisrujana22241a0409:jT6xQF1c44icRNoC@cluster0.nnozvxx.mongodb.net/grietdb?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.nnozvxx.mongodb.net/grietdb?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
     app.listen(8080, () => {
       console.log("Server is running on port 8080");
